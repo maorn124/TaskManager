e4980c1fdd3e12e2f5b802b2657f66376bcff9c1
maorn124 <maor.niv@gmail.com>
2024-07-06 00:19:55 -0400

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Maor Niv on 7/6/24.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
